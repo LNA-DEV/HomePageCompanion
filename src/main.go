cce40765207db3154b1c7844dfa8b329c0d04e99
Lukas Nagel <github@lna-dev.net>
2025-05-27 21:07:09 +0200

    ApiKey validation

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/LNA-DEV/HomePageCompanion/src/autouploader"
 	"github.com/LNA-DEV/HomePageCompanion/src/config"
 	"github.com/LNA-DEV/HomePageCompanion/src/database"
@@ -20,7 +22,7 @@ func main() {
 	router := gin.Default()
 
 	router.POST("/webmention", webmention.HandleWebmention)
-	router.POST("/upload/:platform", uploadNext)
+	router.POST("/upload/:platform", validateAPIKey(), uploadNext)
 
 	router.Run(":8080")
 }
@@ -28,4 +30,19 @@ func main() {
 func uploadNext(c *gin.Context) {
 	platform := c.Param("platform")
 	autouploader.Publish(platform)
+}
+
+func validateAPIKey() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authentication := c.Request.Header.Get("Authorization")
+		expectedAuth := "ApiKey " + config.Data.Security.ApiKey
+
+		if authentication != expectedAuth {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": http.StatusUnauthorized, "message": "Authentication failed"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
 }
